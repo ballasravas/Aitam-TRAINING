d35761045aca6af88ea13b44835b57913d316532
ballasravas <ballasravanthi2004@gmail.com>
2025-01-11 19:38:15 +0530

    mini yoga site

---
+++ test/pra.js
@@ -0,0 +1,27 @@
+/*const RollNo=06;
+console.log(RollNo);
+
+const RollNo=99;
+console.log(RollNo);*/ //reassining is notallowed//redeclarred is not allowed
+
+
+
+/*let stdName="Meeh";
+console.log(stdName);
+
+let stdName="Heeh";
+console.log(stdName);
+stdName="heeh";
+console.log(stdName);*///reassining is allowed//redeclarred is not allowed
+
+/*var stdAge=20;
+console.log(stdAge);
+ var stdAge=21;
+ console.log(stdAge);
+ stdAge=30;
+ console.log(stdAge);*/
+
+/*console.log(stdName);
+var stdName="shu";*/  //undifined
+/*console.log(stdName);
+let stdName="shu";8/ //reference erorr
---
+++ test/var.js
@@ -0,0 +1,8 @@
+const accountId=1234;
+let accountName="John DOE"
+var accountBalance=1000;
+accountAddress="ap";
+
+
+
+console.log({accountId,accountName,accountBalance,accountAddress});7
